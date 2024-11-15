5a503f37b8afb0bc1fbc54eeebc978272b78451c
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-11-14 20:46:50 -0500

    Give vehicle basic movement with acceleration

--- scenes/vehicle/vehicle.gd
+++ scenes/vehicle/vehicle.gd
@@ -1,1 +1,73 @@
-extends CharacterBody3D
+extends AnimatableBody3D
+
+const MAX_LINEAR_SPEED = 3 # m/sec
+const LINEAR_ACCELERATION = 3 # m/sec^2
+const LINEAR_DECELERATION = 12 # m/sec^2
+
+const MAX_ROTATION_SPEED = 0.1 # rot/sec
+const ROTATION_ACCELERATION = 0.2 # rot/sec^2
+const ROTATION_DECELERATION = 0.3 # rot/sec^2
+
+var _drive_input := 0
+var _turn_input := 0
+
+var _current_linear_velocity = 0.0
+var _current_rotational_velocity = 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action("forward") or event.is_action("backward"):
+		_drive_input = int(Input.get_axis("backward", "forward"))
+	if event.is_action("turn_left") or event.is_action("turn_right"):
+		_turn_input = int(Input.get_axis("turn_right", "turn_left"))
+	
+
+
+func _physics_process(delta: float) -> void:
+	_turn(delta)
+	_drive(delta)
+
+
+func _drive(delta: float) -> void:
+	var forward_direction = -transform.basis.z
+	var velocity = _calculate_linear_velocity(delta)
+	position += forward_direction * velocity * delta
+	_current_linear_velocity = velocity
+
+
+func _calculate_linear_velocity(delta: float) -> float:
+	return _calculate_velocity(
+		delta, MAX_LINEAR_SPEED, _drive_input, _current_linear_velocity,
+		LINEAR_ACCELERATION, LINEAR_DECELERATION
+	)
+
+
+func _turn(delta: float) -> void:
+	var velocity = _calculate_rotational_velocity(delta)
+	rotation.y += velocity * TAU * delta
+	_current_rotational_velocity = velocity
+
+
+func _calculate_rotational_velocity(delta: float) -> float:
+	return _calculate_velocity(
+		delta, MAX_ROTATION_SPEED, _turn_input, _current_rotational_velocity,
+		ROTATION_ACCELERATION, ROTATION_DECELERATION
+	)
+
+
+func _calculate_velocity(
+	delta: float, max_speed: float, input: float, current: float,
+	acceleration: float, deceleration: float
+) -> float:
+	var target_velocity = input * max_speed
+	var target_direction = sign(target_velocity - current)
+	# Check if we are decelerating
+	var chosen_acceleration = acceleration if (
+		signf(current) == target_direction
+	) else deceleration
+	
+	var velocity = current + target_direction * chosen_acceleration * delta
+	var resulting_target_direction = sign(target_velocity - velocity)
+	if resulting_target_direction != target_direction:
+		velocity = target_velocity
+	return velocity
